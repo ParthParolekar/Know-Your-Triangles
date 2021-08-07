c4189c0d9804879c48d5907baecda7d82140a09c
ParthParolekar <parolekarparth@gmail.com>
2021-08-07 23:47:19 +0530

    completed Hypotenuse page

---
+++ Hypotenuse/hypotenuse.js
@@ -0,0 +1,15 @@
+const userInput = document.querySelectorAll(".number-input");
+const form = document.querySelector(".hypotenuse-form");
+const result = document.querySelector(".result");
+
+form.addEventListener("submit", calculateHypotenuse);
+
+function calculateHypotenuse(e) {
+  e.preventDefault();
+  let hypotenuse = Math.sqrt(
+    userInput[0].value * userInput[0].value +
+      userInput[1].value * userInput[1].value
+  );
+
+  result.innerText = `AC = ${hypotenuse.toFixed(2)}`;
+}
